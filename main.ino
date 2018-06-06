46b32525e3e88d44687b32cbad560753a4418b4a
atrantan <atrantan@gmail.com>
2018-06-06 11:02:35 +0200

    Ajout fichiers ultrason

--- main.ino
+++ main.ino
@@ -1,30 +1,20 @@
 #include "Arduino.h"
-#include "include/Grove_I2C_Motor_Driver.h"
+#include "include/Ultrasonic.h"
 
-// default I2C address is 0x0A
-#define I2C_ADDRESS 0x0A
+Ultrasonic ultrasonic(2);
 
 void setup()
 {
-    Motor.begin(I2C_ADDRESS);
 }
 
 void loop()
 {
-    // Set speed of MOTOR1, Clockwise
-    Motor.speed(MOTOR1, 50);
-    // Set speed of MOTOR2, Anticlockwise
-    Motor.speed(MOTOR2, -70);
-    delay(2000);
+    long RangeInInches;
+    long RangeInCentimeters;
 
-    // Change speed and direction of MOTOR1
-    Motor.speed(MOTOR1, -100);
-    // Change speed and direction of MOTOR2
-    Motor.speed(MOTOR2, 100);
-    delay(2000);
+    RangeInInches = ultrasonic.MeasureInInches();
+    delay(250);
 
-    // Stop MOTOR1 and MOTOR2
-    Motor.stop(MOTOR1);
-    Motor.stop(MOTOR2);
-    delay(2000);
+    RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+    delay(250);
 }
