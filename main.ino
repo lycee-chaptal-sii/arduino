2df85342cc4a1a8bc256aaec0d99bcb4355f0b91
atrantan <atrantan@gmail.com>
2018-06-06 11:03:54 +0200

    Ajout copyright

--- main.ino
+++ main.ino
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 //  Copyright (c) 2019 Antoine Tran Tan
+=======
+//  Copyright (c) 2018 Antoine Tran Tan
+>>>>>>> Ajout copyright
 //
 
 #include "Arduino.h"
@@ -18,6 +22,14 @@ void loop()
     RangeInInches = ultrasonic.MeasureInInches();
     delay(250);
 
+<<<<<<< HEAD
     RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
     delay(250);
+=======
+    // Stop MOTOR1 and MOTOR2
+    Motor.stop(MOTOR1);
+    Motor.stop(MOTOR2);
+    delay(2000);
+
+>>>>>>> Ajout copyright
 }
